92f5848e1fae63efe34b07129efc00bf42e388a5
Benoit Habfast <habfast.benoit@gmail.com>
2017-05-20 09:14:12 +0100

    home page lnk added

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :ingredients
   mount Attachinary::Engine => "/attachinary"
 
+  get '/', to: 'cocktails#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
